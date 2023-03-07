6e939393aaefcb487e3154f81ad023d3a9401e7a
Tselmen Azjargal <110718202+skrTR@users.noreply.github.com>
2023-03-08 06:45:34 +0800

    a

--- src/common/MetaShare.js
+++ src/common/MetaShare.js
@@ -18,6 +18,8 @@ const MetaShare = ({ title, ogTitle, ogDescrtiption, image }) => {
         key="og:description"
         content={ogDescrtiption}
       />
+      <meta property="og:image:width" content="1280" />
+<meta property="og:image:height" content="855" />
       <meta
         property="og:image"
         key="og:image"
