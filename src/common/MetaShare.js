c91475192400262cea02fcca627d3128fd563118
Tselmen Azjargal <110718202+skrTR@users.noreply.github.com>
2023-03-08 06:48:22 +0800

    a

--- src/common/MetaShare.js
+++ src/common/MetaShare.js
@@ -5,21 +5,19 @@ const MetaShare = ({ title, ogTitle, ogDescrtiption, image }) => {
   return (
     <Helmet>
       <title>{title}</title>
+      {/* <meta property="og:type"               content="article" data-react-helmet="true" />
+    <meta property="og:title"              content="Read, Think, decide." data-react-helmet="true"/>
+    <meta property="og:description"        content="Үнэ цэнтэй, чанартай мэдээлэл хүргэхийн тулд бид улам бүр хичээн ажиллах болно." data-react-helmet="true"/>
+    <meta property="og:image" content="https://career-developer.com/upload/emuna1.png" data-react-helmet="true"/> */}
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <meta name="description" key="description" content={ogDescrtiption} />
       <meta name="title" key="title" content={title} />
+      <meta property="og:type"               content="article" data-react-helmet="true" />
       <meta property="og:title" key="og:title" content={ogTitle} />
+      <meta property="og:description"   content={ogDescrtiption}     />
       <meta property="og:locale" key="og:locale" content="en_US" />
       <meta charSet="utf-8" />
-      <meta property="og:type" key="og:type" content="website" />
-      <meta
-        property="og:description"
-        key="og:description"
-        content={ogDescrtiption}
-      />
-      <meta property="og:image:width" content="1280" />
-<meta property="og:image:height" content="855" />
       <meta
         property="og:image"
         key="og:image"
