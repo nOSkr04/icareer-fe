9ec99e5981ba268094b4341f57cd40f83bfac1a3
nOSkr <118327643+nOSkr@users.noreply.github.com>
2023-03-09 17:23:15 +0800

    done

--- src/components/news/NewsDetailsContent.js
+++ src/components/news/NewsDetailsContent.js
@@ -3,82 +3,81 @@ import React from "react";
 import SEO from "../../common/SEO";
 const NewsDetailsContent = (props) => {
   const { data } = props;
+  if (!data) {
+    return null;
+  }
   return (
-    <>
-      {data && (
-        <div>
-          <SEO title={data.title} description="" image={data.image} />
-          <div className="post-page-banner rn-section-gapTop">
-            <div className="container">
-              <div className="row">
-                <div className="col-lg-8 offset-lg-2">
-                  <div className="content text-center">
-                    <div className="page-title">
-                      <h1 className="theme-gradient">{data.title}</h1>
-                    </div>
-
-                    <div className="thumbnail alignwide mt--60">
-                      <img
-                        className="w-100 radius"
-                        src={`https://career-developer.com/upload/${data.image}`}
-                        alt="Blog Images"
-                      />
-                    </div>
-                  </div>
+    <div>
+      <SEO title={data.title} description="" image={data.image} />
+      <div className="post-page-banner rn-section-gapTop">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-8 offset-lg-2">
+              <div className="content text-center">
+                <div className="page-title">
+                  <h1 className="theme-gradient">{data.title}</h1>
                 </div>
-              </div>
-            </div>
-          </div>
-          <div className="blog-details-content pt--60 rn-section-gapBottom">
-            <div className="container">
-              <div className="row">
-                <div className="col-lg-8 offset-lg-2">
-                  <div className="content">
-                    {data.body &&
-                      data.body.map((value, i) => {
-                        return (
-                          <div
-                            key={i}
-                            dangerouslySetInnerHTML={{ __html: value }}
-                          />
-                        );
-                      })}
-                    {data.body1 &&
-                      data.body1.map((value, i) => {
-                        return (
-                          <div
-                            key={i}
-                            dangerouslySetInnerHTML={{ __html: value }}
-                          />
-                        );
-                      })}
-                    {data.body2 &&
-                      data.body2.map((value, i) => {
-                        return (
-                          <div
-                            key={i}
-                            dangerouslySetInnerHTML={{ __html: value }}
-                          />
-                        );
-                      })}
-                    <iframe
-                      src={`https://www.facebook.com/plugins/like.php?href=https://icareer.mn/news/${data._id}&width=174&layout=button_count&action=like&size=large&share=true&height=46&appId=662546334827488`}
-                      width={174}
-                      height={46}
-                      style={{ border: "none", overflow: "hidden" }}
-                      scrolling="no"
-                      frameborder="0"
-                      allowfullscreen="true"
-                      allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
-                    ></iframe>
-                  </div>
+
+                <div className="thumbnail alignwide mt--60">
+                  <img
+                    className="w-100 radius"
+                    src={`https://career-developer.com/upload/${data.image}`}
+                    alt="Blog Images"
+                  />
                 </div>
               </div>
             </div>
           </div>
         </div>
-      )}
-    </>
+      </div>
+      <div className="blog-details-content pt--60 rn-section-gapBottom">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-8 offset-lg-2">
+              <div className="content">
+                {data.body &&
+                  data.body.map((value, i) => {
+                    return (
+                      <div
+                        key={i}
+                        dangerouslySetInnerHTML={{ __html: value }}
+                      />
+                    );
+                  })}
+                {data.body1 &&
+                  data.body1.map((value, i) => {
+                    return (
+                      <div
+                        key={i}
+                        dangerouslySetInnerHTML={{ __html: value }}
+                      />
+                    );
+                  })}
+                {data.body2 &&
+                  data.body2.map((value, i) => {
+                    return (
+                      <div
+                        key={i}
+                        dangerouslySetInnerHTML={{ __html: value }}
+                      />
+                    );
+                  })}
+                <iframe
+                  src={`https://www.facebook.com/plugins/like.php?href=https://icareer.mn/news/${data._id}&width=174&layout=button_count&action=like&size=large&share=true&height=46&appId=662546334827488`}
+                  width={174}
+                  height={46}
+                  style={{ border: "none", overflow: "hidden" }}
+                  scrolling="no"
+                  frameborder="0"
+                  allowfullscreen="true"
+                  allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+                ></iframe>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
